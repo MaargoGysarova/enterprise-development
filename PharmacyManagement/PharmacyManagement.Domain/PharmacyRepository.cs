f6bbcf930cac6f3fb513340f52fba976e2549d5f
Маргарита <113328619+MaargoGysarova@users.noreply.github.com>
2024-11-19 13:30:02 +0400

    xml-комментарий в Pep

--- PharmacyManagement/PharmacyManagement.Domain/PharmacyRepository.cs
+++ PharmacyManagement/PharmacyManagement.Domain/PharmacyRepository.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using PharmacyManagementSystem;
 
-
 namespace PharmacyManagementSystem
 {
+    /// <summary>
+    /// Класс, представляющий репозиторий аптек и предоставляющий методы для управления аптеками и препаратами.
+    /// </summary>
     public class PharmacyRepository
     {
+        /// <summary>
+        /// Коллекция всех аптек, хранимая в виде словаря, где ключ - идентификатор аптеки.
+        /// </summary>
         public Dictionary<int, Pharmacy> Pharmacies { get; set; } = new Dictionary<int, Pharmacy>();
 
-        // Метод для добавления аптеки
+        /// <summary>
+        /// Добавить новую аптеку в репозиторий.
+        /// </summary>
+        /// <param name="pharmacy">Аптека для добавления.</param>
         public void AddPharmacy(Pharmacy pharmacy)
         {
             Pharmacies[pharmacy.PharmacyId] = pharmacy;
         }
 
-        // Метод для получения списка всех аптек
+        /// <summary>
+        /// Получить список всех аптек из репозитория.
+        /// </summary>
+        /// <returns>Список всех аптек.</returns>
         public List<Pharmacy> GetAllPharmacies()
         {
             return Pharmacies.Values.ToList();
         }
 
-        // Метод для добавления лекарства в аптеку через прайс-лист
+        /// <summary>
+        /// Добавить препарат в аптеку через прайс-лист.
+        /// </summary>
+        /// <param name="pharmacyId">Идентификатор аптеки, в которую добавляется препарат.</param>
+        /// <param name="priceList">Прайс-лист с информацией о препарате.</param>
         public void AddMedicineToPharmacy(int pharmacyId, PriceList priceList)
         {
             if (Pharmacies.TryGetValue(pharmacyId, out var pharmacy))
@@ -31,7 +46,11 @@ namespace PharmacyManagementSystem
             }
         }
 
-        // 1. Вывести сведения о всех препаратах в заданной аптеке, упорядочить по названию
+        /// <summary>
+        /// Получить сведения о всех препаратах в указанной аптеке, отсортированных по названию.
+        /// </summary>
+        /// <param name="pharmacyId">Идентификатор аптеки.</param>
+        /// <returns>Список препаратов, отсортированный по названию.</returns>
         public List<Medicine> GetAllMedicinesInPharmacy(int pharmacyId)
         {
             if (!Pharmacies.TryGetValue(pharmacyId, out var pharmacy))
@@ -42,7 +61,11 @@ namespace PharmacyManagementSystem
             return pharmacy.PriceLists.Select(pl => pl.Medicine).OrderBy(m => m.Name).ToList();
         }
 
-        // 2. Вывести для данного препарата подробный список всех аптек с указанием количества препарата в аптеках
+        /// <summary>
+        /// Получить подробный список всех аптек, в которых имеется указанный препарат, с указанием его количества.
+        /// </summary>
+        /// <param name="medicineName">Название препарата.</param>
+        /// <returns>Список кортежей, содержащих аптеку и количество препарата в ней.</returns>
         public List<(Pharmacy Pharmacy, int Quantity)> GetPharmaciesWithMedicineInfo(string medicineName)
         {
             return Pharmacies.Values
@@ -51,19 +74,27 @@ namespace PharmacyManagementSystem
                 .ToList();
         }
 
-        // 3. Вывести информацию о средней стоимости препаратов каждой фармацевтической группе для каждой аптеки
-       public List<(Pharmacy Pharmacy, string PharmaceuticalGroup, decimal AveragePrice)> GetAveragePriceByPharmaceuticalGroup()
-{
-    return Pharmacies.Values.SelectMany(pharmacy =>
-        pharmacy.PriceLists
-            .SelectMany(pl => pl.Medicine.PharmaceuticalGroups, (pl, group) => new { Pharmacy = pharmacy, Group = group.ToString(), pl.Price }) // Преобразуем группу в строку
-            .GroupBy(x => new { x.Pharmacy, x.Group })
-            .Select(g => (g.Key.Pharmacy, g.Key.Group, AveragePrice: g.Average(x => x.Price)))
-    ).ToList();
-}
+        /// <summary>
+        /// Получить информацию о средней стоимости препаратов каждой фармацевтической группы для каждой аптеки.
+        /// </summary>
+        /// <returns>Список кортежей, содержащих аптеку, фармацевтическую группу и среднюю стоимость препаратов в данной группе.</returns>
+        public List<(Pharmacy Pharmacy, string PharmaceuticalGroup, decimal AveragePrice)> GetAveragePriceByPharmaceuticalGroup()
+        {
+            return Pharmacies.Values.SelectMany(pharmacy =>
+                pharmacy.PriceLists
+                    .SelectMany(pl => pl.Medicine.PharmaceuticalGroups, (pl, group) => new { Pharmacy = pharmacy, Group = group.ToString(), pl.Price }) // Преобразуем группу в строку
+                    .GroupBy(x => new { x.Pharmacy, x.Group })
+                    .Select(g => (g.Key.Pharmacy, g.Key.Group, AveragePrice: g.Average(x => x.Price)))
+            ).ToList();
+        }
 
-
-        // 4. Вывести топ 5 аптек по количеству и объёму продаж данного препарата за указанный период времени
+        /// <summary>
+        /// Получить топ-5 аптек по количеству и объему продаж указанного препарата за указанный период времени.
+        /// </summary>
+        /// <param name="medicineName">Название препарата.</param>
+        /// <param name="startDate">Начальная дата периода.</param>
+        /// <param name="endDate">Конечная дата периода.</param>
+        /// <returns>Список кортежей, содержащих аптеку, количество проданных препаратов и общую сумму продаж.</returns>
         public List<(Pharmacy Pharmacy, int QuantitySold, decimal TotalSales)> GetTop5PharmaciesBySales(string medicineName, DateTime startDate, DateTime endDate)
         {
             return Pharmacies.Values
@@ -79,7 +110,13 @@ namespace PharmacyManagementSystem
                 .ToList();
         }
 
-        // 5. Вывести список аптек указанного района, продавших заданный препарат более указанного объёма
+        /// <summary>
+        /// Получить список аптек, расположенных в указанном районе, которые продали указанный препарат в объеме больше заданного.
+        /// </summary>
+        /// <param name="region">Район, в котором находятся аптеки.</param>
+        /// <param name="medicineName">Название препарата.</param>
+        /// <param name="minQuantity">Минимальный объем продажи.</param>
+        /// <returns>Список аптек, соответствующих указанным критериям.</returns>
         public List<Pharmacy> GetPharmaciesByRegionAndQuantity(string region, string medicineName, int minQuantity)
         {
             return Pharmacies.Values
@@ -87,7 +124,11 @@ namespace PharmacyManagementSystem
                 .ToList();
         }
 
-        // 6. Вывести список аптек, в которых указанный препарат продается с минимальной ценой
+        /// <summary>
+        /// Получить список аптек, в которых указанный препарат продается по минимальной цене.
+        /// </summary>
+        /// <param name="medicineName">Название препарата.</param>
+        /// <returns>Список аптек, продающих препарат по минимальной цене.</returns>
         public List<Pharmacy> GetPharmaciesWithMinimumPrice(string medicineName)
         {
             var minPrice = Pharmacies.Values
